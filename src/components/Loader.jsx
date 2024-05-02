79a4ddf9efdc940a15fe1cde3647b2e74ff34814
unknown <khuranavishal000@gmail.com>
2024-05-02 15:28:30 +0530

    loader style changed

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,19 +1,28 @@
-import React from "react";
 import { Html, useProgress } from "@react-three/drei";
+
 const CanvasLoader = () => {
   const { progress } = useProgress();
   return (
-    <Html>
-      <span className="canvas-load"></span>
+    <Html
+      as="div"
+      center
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <span className="canvas-loader"></span>
       <p
         style={{
           fontSize: 14,
-          color: "#f1f1f1",
-          fontWeight: 100,
+          color: "#F1F1F1",
+          fontWeight: 800,
           marginTop: 40,
         }}
       >
-        Loading&nbsp;{progress.toFixed(2)}%
+        {progress.toFixed(2)}%
       </p>
     </Html>
   );
